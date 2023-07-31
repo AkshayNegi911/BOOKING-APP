c95a7a84b9a79ed788898a0ee69481c76a883797
AkshayNegi911 <akshaynegi912@gmail.com>
2023-07-31 18:18:56 +0530

    cors

--- API/index.js
+++ API/index.js
@@ -35,7 +35,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 
 
 app.use(cors({
-  origin: 'https://booking-app-jade-phi.vercel.app/', // Replace with your web app's domain
+  origin: true, // Replace with your web app's domain
   credentials: true, // Allow requests to include credentials (e.g., cookies, authorization headers)
 }));
 
