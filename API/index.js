83b8f90f40f7382e6f691cd286065f38e8a02727
AkshayNegi911 <akshaynegi912@gmail.com>
2024-01-13 00:47:01 +0530

    vercel #6

--- API/index.js
+++ API/index.js
@@ -46,7 +46,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: ["https://booking-app-frontend-wine.vercel.app/"],
+    origin: ["https://booking-app-frontend-wine.vercel.app"],
   })
 )
 
